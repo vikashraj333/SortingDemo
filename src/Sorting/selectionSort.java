f85d94343fe32644be6208cbad400872ae4da26a
vikashraj333 <rajputvikash333@gmail.com>
2022-03-15 10:37:39 +0530

    Sorting

---
+++ src/Sorting/selectionSort.java
@@ -0,0 +1,32 @@
+package Sorting;
+
+public class selectionSort {
+
+	public static void sort(int[] arr) {
+		for(int i=0;i<arr.length-1;i++) {
+			int index=i;
+			for(int j=i+1;j<arr.length;j++) {
+				if(arr[j]<arr[index]) 
+					index=j;
+			}
+			int smallerNumber=arr[index];
+			arr[index]=arr[i];
+			arr[i]=smallerNumber;
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] arr1= {9,14,3,2,43,11,58,22};
+		System.out.println("Beforing selection sorting : ");
+		for(int i:arr1)
+			System.out.print(i+" ");
+		System.out.println();
+		
+		sort(arr1);
+		
+		System.out.println("After selection sorting : ");
+		for(int i:arr1)
+			System.out.print(i+" ");
+		System.out.println();
+	}
+}
